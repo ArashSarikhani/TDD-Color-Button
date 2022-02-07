dd0868040a85d53b76d2c9658b3d038e28132b9e
Arash Sa <saeidalirezaei@Saeids-iMac.local>
2022-02-07 09:24:42 +0330

    changeColorButton-to-Blue

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByRole("link", {
+//     name: /learnig testing library/i,
+//   });
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole("button", {
+    name: "change to blue",
+  });
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  expect(colorButton.textContent).toBe("change to red");
 });
